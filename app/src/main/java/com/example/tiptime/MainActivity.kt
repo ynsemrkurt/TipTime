8cd08cad2f1c2a568a1c0f19bc22e716acf98e89
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-09 12:31:05 +0300

    Add vertical option

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -30,7 +30,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -76,7 +78,9 @@ fun TipTimeLayout() {
     var roundUp by remember { mutableStateOf(false) }
     val tip = calculateTip(amount, tipPercent, roundUp = roundUp)
     Column(
-        modifier = Modifier.padding(40.dp),
+        modifier = Modifier
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -160,7 +164,7 @@ fun RoundTheTipRow(
                 .fillMaxWidth()
                 .wrapContentWidth(Alignment.End),
             checked = roundUp,
-            onCheckedChange = onRoundUpChanged,
+            onCheckedChange = onRoundUpChanged
         )
     }
 }
